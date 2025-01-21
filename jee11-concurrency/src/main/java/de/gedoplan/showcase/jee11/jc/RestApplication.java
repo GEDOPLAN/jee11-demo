6863a9932057824be2b41a3c14bb5bf3912d9a15
Dirk Weil <dirk.weil@gedoplan.de>
2025-01-21 14:37:08 +0100

    Unterprojekte zu Concurrency konsolidiert

--- jee11-concurrency/src/main/java/de/gedoplan/showcase/jee11/jc/RestApplication.java
+++
@@ -1,8 +0,0 @@
-package de.gedoplan.showcase.jee11.jc;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/")
-public class RestApplication extends Application {
-}
