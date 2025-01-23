9ba7180daa49eee172386af923534cc75d881843
Markus Pauer <markus.pauer@gedoplan.de>
2025-01-23 10:35:04 +0100

    Endpunkt erweitert

--- jee11-persistence/src/main/java/de/gedoplan/showcase/jee11/jp/rest/PersonResource.java
+++ jee11-persistence/src/main/java/de/gedoplan/showcase/jee11/jp/rest/PersonResource.java
@@ -13,7 +13,10 @@ import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.UriInfo;
 import lombok.extern.java.Log;
+import org.hibernate.query.sqm.spi.JdbcParameterBySqmParameterAccess;
 
+import java.time.Year;
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 import java.util.function.BiConsumer;
@@ -23,42 +26,33 @@ import java.util.function.BiConsumer;
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class PersonResource {
+  // Injektion der UriInfo erfolgt nun über @Inject statt @Context
   @Inject
   UriInfo uriInfo;
-
-  @Inject
-  Validator validator;
 
   @Inject
   PersonRepository personRepository;
 
   @GET
-  public List<Person> getPersons() {
-    return personRepository.findAll();
+  public List<Person> getPersons(@DefaultValue("default") @QueryParam("type") String type) {
+    return switch (type) {
+      case "short" -> personRepository.findAllShortSyntax();
+      case "save" -> personRepository.findAllTypeSave();
+      case "default" -> personRepository.findAll();
+      default -> Collections.emptyList();
+    };
   }
 
-  /**
-   * Using Jakarta RESTful Web Servic to check Person for Constraint Violations
-   */
-  @POST
-  @Path("check")
-  public Response checkPerson(@Valid Person person) {
-    log.info("Check Person: " + person);
-    return Response.ok().build();
+  @GET
+  @Path("{name}")
+  public Person getPerson(@PathParam("name") String name) {
+    return personRepository.findByName(name);
   }
 
-  /**
-   * Using Jakarta Validation to check Person for Constraint Violations
-   */
-  @POST
-  @Path("validate")
-  public Response validatePerson(Person person) {
-    log.info("Validate Person: " + person);
-    var violations = validator.validate(person);
-    if (!violations.isEmpty()) {
-      return createResponse(Response.status(Response.Status.BAD_REQUEST), violations);
-    }
-    return Response.ok().build();
+  @GET
+  @Path("{name}/reg")
+  public Integer getYearOfRegistration(@PathParam("name") String name) {
+    return personRepository.getRegistrationYear(name);
   }
 
   @POST
@@ -67,7 +61,7 @@ public class PersonResource {
     try {
       personRepository.save(person);
     } catch (ConstraintViolationException exception) {
-      return Response.status(Response.Status.BAD_REQUEST).header("persistence-validation-exception", exception.getLocalizedMessage()).build();
+      return createResponse(Response.status(Response.Status.BAD_REQUEST), exception.getConstraintViolations());
     }
     return Response.created(
             uriInfo
@@ -77,7 +71,7 @@ public class PersonResource {
         .build();
   }
 
-  private Response createResponse(Response.ResponseBuilder responseBuilder, Set<ConstraintViolation<Person>> violations) {
+  private Response createResponse(Response.ResponseBuilder responseBuilder, Set<ConstraintViolation<?>> violations) {
     BiConsumer<String, String> addHeader = responseBuilder::header;
     violations.forEach(violation -> addHeader.accept(violation.getPropertyPath().toString(), violation.getMessage()));
     return responseBuilder.build();
